3ddb203f0ad7f18fa5329bc446a857b3dcdc5a2c
Vanja Petkovic <vanja.petkovic@gmail.com>
2024-04-27 15:44:51 -0700

    Initial commit

---
+++ data-structures/linked-lists/SinglyLinkedList.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+}
