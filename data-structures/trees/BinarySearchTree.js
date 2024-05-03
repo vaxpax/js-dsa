2eeb348360c6d6ce165f64b9db5798d82a7099d1
Vanja Petkovic <vanja.petkovic@gmail.com>
2024-05-02 21:02:28 -0700

    BinarySearchTree methods toInOrderArray, toReverseOrderArray, search, min, max

--- data-structures/trees/BinarySearchTree.js
+++ data-structures/trees/BinarySearchTree.js
@@ -3,8 +3,8 @@
 import { defaultCompare } from "../../utils/Utils.js";
 
 class TreeNode {
-    constructor(value) {
-        this.value = value;
+    constructor(key) {
+        this.key = key;
         this.left = null;
         this.right = null;
     }
@@ -20,25 +20,100 @@ class BinarySearchTree {
         return this.root === null;
     }
 
-    insert(value) {
-        this.root = this.insertValue(this.root, value);
+    insert(key) {
+        this.root = this.insertValue(this.root, key);
     }
 
-    insertAll(valuesArray) {
-        for (let value of valuesArray) {
-            this.insert(value);
+    insertAll(keysArray) {
+        for (let key of keysArray) {
+            this.insert(key);
         }
     }
 
-    insertValue(node, value) {
+    insertValue(node, key) {
         if (node === null) {
-            node = new TreeNode(value);
+            node = new TreeNode(key);
             return node;
         }
-        if (this.compare(value, node.value) < 0) {
-            node.left = this.insertValue(node.left, value);
-        } else if (this.compare(value, node.value) > 0) {
-            node.right = this.insertValue(node.right, value)
+        if (this.compare(key, node.key) < 0) {
+            node.left = this.insertValue(node.left, key);
+        } else if (this.compare(key, node.key) > 0) {
+            node.right = this.insertValue(node.right, key)
+        }
+        return node;
+    }
+
+    toInOrderArray() {
+        const array = [];
+        if (this.root != null) {
+            this.inOrderTreeWalk(this.root, array);
+        }
+        return  array;
+    }
+
+    inOrderTreeWalk(node, array) {
+        if (node != null) {
+            this.inOrderTreeWalk(node.left, array);
+            array.push(node.key);
+            this.inOrderTreeWalk(node.right, array);
+        }
+    }
+
+    toReverseOrderArray() {
+        const array = [];
+        if (this.root != null) {
+            this.reverseOrderTreeWalk(this.root, array);
+        }
+        return  array;
+    }
+
+    reverseOrderTreeWalk(node, array) {
+        if (node != null) {
+            this.reverseOrderTreeWalk(node.right, array);
+            array.push(node.key);
+            this.reverseOrderTreeWalk(node.left, array);
+        }
+    }
+
+    search(key) {
+        return this.searchNode(this.root, key);
+    }
+
+    searchNode(node, key) {
+        if (node == null || key === node.key) {
+            return node;
+        }
+        if (this.compare(key, node.key) < 0 ) {
+            return this.searchNode(node.left, key);
+        } else {
+            return this.searchNode(node.right, key);
+        }
+    }
+
+    min() {
+        if (this.root == null) {
+            return null;
+        }
+        return this.minNode(this.root);
+    }
+
+    minNode(node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    max() {
+        if (this.root == null) {
+            return null;
+        }
+        return this.maxNode(this.root);
+    }
+
+    maxNode(node) {
+        while (node.right != null) {
+            node = node.right;
         }
         return node;
     }
--- test/data-structures/trees/BinarySearchTree.test.js
+++ test/data-structures/trees/BinarySearchTree.test.js
@@ -1,12 +1,13 @@
 "use strict";
 
 import { assert } from "chai";
-import { BinarySearchTree} from "../../../data-structures/trees/BinarySearchTree.js";
+import { assertArrays } from "../../TestHelpers.js";
+import { BinarySearchTree, TreeNode } from "../../../data-structures/trees/BinarySearchTree.js";
 
 
 
 export default function test() {
-    describe('CircularLinkedList Tests', () => {
+    describe('BinarySearchTree Tests', () => {
         it('create an empty tree', () => {
             const tree = new BinarySearchTree();
             assert.equal(tree.isEmpty(), true);
@@ -15,17 +16,40 @@ export default function test() {
             const tree = new BinarySearchTree();
             tree.insert(5);
             assert.equal(tree.isEmpty(), false);
-            assert.equal(tree.root.value, 5);
+            assert.equal(tree.root.key, 5);
         });
-        it('insertAll', () => {
+        it('insertAll, toInOrderArray, toReverseOrderArray', () => {
             const tree = new BinarySearchTree();
             const array = [4, 5, 2, 3, 6, 1];
             tree.insertAll(array);
             assert.equal(tree.isEmpty(), false);
-            assert.equal(tree.root.value, 5);
+            assert.equal(tree.root.key, 4);
+            assertArrays(array.sort(), tree.toInOrderArray());
+            assertArrays(array.reverse(), tree.toReverseOrderArray());
         });
 
+        it('search', () => {
+            const tree = new BinarySearchTree();
+            const array = [4, 5, 2, 3, 6, 1];
+            tree.insertAll(array);
+            assert.equal(4, tree.search(4).key)
+            assert.equal(1, tree.search(1).key)
+            assert.equal(3, tree.search(3).key)
+        });
 
+        it('min', () => {
+            const tree = new BinarySearchTree();
+            const array = [4, 5, 2, 3, 6, 1];
+            tree.insertAll(array);
+            assert.equal(tree.min().key, 1);
+        });
+
+        it('max', () => {
+            const tree = new BinarySearchTree();
+            const array = [4, 5, 2, 3, 6, 1];
+            tree.insertAll(array);
+            assert.equal(tree.max().key, 6);
+        });
     });
 }
 
