efe35bb2896a764618f48a297e03edf6938d3a07
Vanja Petkovic <vanja.petkovic@gmail.com>
2024-05-08 22:24:52 -0700

    AVLTree clean code

--- data-structures/trees/AVLTree.js
+++ data-structures/trees/AVLTree.js
@@ -72,25 +72,6 @@ class AVLTree extends BinarySearchTree {
         return this.balance(node);
     }
 
-    balance(node) {
-        node.height = 1 + Math.max(AVLTreeNode.height(node.left), AVLTreeNode.height(node.right));
-        const balance = this.getBalance(node);
-
-        if (this.getBalance(node) > 1) {
-            if (this.getBalance(node.left) < 0) {
-                node.left = this.leftRotate(node.left);
-            }
-            return this.rightRotate(node);
-        }
-        if (this.getBalance(node) < -1) {
-            if (this.getBalance(node.right) > 0) {
-                node.right = this.rightRotate(node.right);
-            }
-            return this.leftRotate(node);
-        }
-        return node;
-    }
-
     delete(key) {
         this.root = this.deleteNode(this.root, key);
     }
@@ -112,12 +93,10 @@ class AVLTree extends BinarySearchTree {
                 } else {
                     temp = root.left;
                 }
-
                 if (temp == null) {
                     temp = root;
                     root = null;
-                }
-                else {
+                } else {
                     root = temp;
                 }
             }
@@ -132,30 +111,26 @@ class AVLTree extends BinarySearchTree {
             return root;
         }
 
-        root.height = Math.max(AVLTreeNode.height(root.left), AVLTreeNode.height(root.right)) + 1;
-        return this.deleteBalance(root);
-
+        return this.balance(root);
     }
 
-    deleteBalance(root) {
-        const balance = this.getBalance(root);
+    balance(node) {
+        node.height = 1 + Math.max(AVLTreeNode.height(node.left), AVLTreeNode.height(node.right));
+        const balance = this.getBalance(node);
 
-        if (balance > 1 && this.getBalance(root.left) >= 0) {
-            return this.rightRotate(root);
+        if (this.getBalance(node) > 1) {
+            if (this.getBalance(node.left) < 0) {
+                node.left = this.leftRotate(node.left);
+            }
+            return this.rightRotate(node);
         }
-        if (balance > 1 && this.getBalance(root.left) < 0) {
-            root.left = this.leftRotate(root.left);
-            return this.rightRotate(root);
+        if (this.getBalance(node) < -1) {
+            if (this.getBalance(node.right) > 0) {
+                node.right = this.rightRotate(node.right);
+            }
+            return this.leftRotate(node);
         }
-        if (balance < -1 && this.getBalance(root.right) <= 0) {
-            return this.leftRotate(root);
-        }
-        if (balance < -1 && this.getBalance(root.right) > 0) {
-            root.right = this.rightRotate(root.right);
-            return this.leftRotate(root);
-        }
-
-        return root;
+        return node;
     }
 
     successor(node) {
