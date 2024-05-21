5fb3e4744a069e555bc23f86db426633df6e8cc5
Vanja Petkovic <vanja.petkovic@gmail.com>
2024-05-20 20:41:29 -0700

    jsdoc update for errors

--- utils/Errors.js
+++ utils/Errors.js
@@ -1,9 +1,19 @@
 "use strict";
 
+/**@namespace errors*/
+
 /**
- * @summary Class representing not implemented error. It is thrown if some method is not implemented in class
+ * @summary Class representing not implemented error.
+ * @classdesc
+ * @memberof errors
  */
 class NotImplementedError extends Error {
+    /**
+     * @class Creates a new NotImplementedError.
+     * @alias NotImplementedError
+     * @param {String} message message for error
+     * @constructor
+     */
     constructor(message) {
         super(message);
         this.name = "NotImplementedError";
@@ -11,9 +21,17 @@ class NotImplementedError extends Error {
 }
 
 /**
- * @summary Class representing not supported error. It is thrown if some method is not supported in class.
+ * @summary Class representing not supported error.
+ * @classdesc
+ * @memberof errors
  */
 class NotSupportedError extends Error {
+    /**
+     * @class Creates a new NotSupportedError.
+     * @alias NotSupportedError
+     * @param {String} message message for error
+     * @constructor
+     */
     constructor(message) {
         super(message);
         this.name = "NotSupportedError";
