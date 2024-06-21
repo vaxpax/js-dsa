2e19dd3ea139a6bd0ca20858e9b230548d0b1886
Vanja Petkovic <vanja.petkovic@gmail.com>
2024-06-20 20:28:12 -0700

    Clean up

--- algorithms/graphs/Dijkstra.js
+++ algorithms/graphs/Dijkstra.js
@@ -5,7 +5,7 @@ import {AdjacencyMatrixGraph} from "../../data-structures/graphs/AdjacencyMatrix
  * @classdesc
  * @memberof graphs
  */
-class  Dijkstra extends AdjacencyMatrixGraph {
+class Dijkstra extends AdjacencyMatrixGraph {
     /**
      * @class Creates a new Dijkstra class.
      * @param verticesNumber - number of vertices in Graph. Default value is 0
@@ -29,37 +29,31 @@ class  Dijkstra extends AdjacencyMatrixGraph {
      */
     search(source) {
         const visited = Array(this.adjacencyMatrix.length).fill(false);
-        const distance = Array(this.adjacencyMatrix.length).fill(Number.MAX_SAFE_INTEGER);
+        const distances = Array(this.adjacencyMatrix.length).fill(Number.MAX_SAFE_INTEGER);
 
-        distance[source] = 0;
+        distances[source] = 0;
         for (let i = 0; i < this.adjacencyMatrix.length; i++) {
-            let u = this.findMinDistance(distance, visited);
+            let u = this._findMinDistance(distances, visited);
             visited[u] = true;
 
-            // Update all the neighbouring vertex distances
+            // Update all the neighboring vertex distances
             for (let v = 0; v < this.adjacencyMatrix.length; v++) {
                 if (!visited[v] && this.adjacencyMatrix[u][v] !== 0
-                        && (distance[u] + this.adjacencyMatrix[u][v] < distance[v])) {
-                    distance[v] = distance[u] + this.adjacencyMatrix[u][v];
+                        && (distances[u] + this.adjacencyMatrix[u][v] < distances[v])) {
+                    distances[v] = distances[u] + this.adjacencyMatrix[u][v];
                 }
             }
         }
 
-        return distance;
+        return distances;
     }
 
-    /**
-     * Method to find minimum distance
-     * @param distance - array of distances
-     * @param visited - array of visited vertices
-     * @returns {number} minimal distance
-     */
-    findMinDistance(distance, visited) {
+    _findMinDistance(distances, visited) {
         let minDistance = Number.MAX_SAFE_INTEGER;
         let minDistanceVertex = -1;
-        for (let i = 0; i < distance.length; i++) {
-            if (!visited[i] && distance[i] < minDistance) {
-                minDistance = distance[i];
+        for (let i = 0; i < distances.length; i++) {
+            if (!visited[i] && distances[i] < minDistance) {
+                minDistance = distances[i];
                 minDistanceVertex = i;
             }
         }
